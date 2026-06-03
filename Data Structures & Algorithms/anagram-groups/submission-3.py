d06b3fccb305268b98aef99b417d027b22f2d801
ron <138569343+ronaldw07@users.noreply.github.com>
2026-06-03 14:38:49 -0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #loop through the array
+
+        #group each string into a sorted word as a key with a hashmap
+
+        # return the values as a list (no keys)
+
+        anagrams = {}
+
+        for word in strs:
+            #change each word to list, then set it to find the key.
+            #but the key must be immutable so make it a tuple 
+
+            sortedlistword = sorted(list(word)) #hat turns into [a,h,t]
+            key = "".join(sortedlistword) #key is aht
+
+            if key in anagrams:
+                anagrams[key].append(word)
+            else:
+                anagrams[key] = [word]
+
+        return list(anagrams.values())
+
+
+
