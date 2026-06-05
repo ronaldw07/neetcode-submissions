eb7247d095021ee0ecfc4e001e07e8a9fbd9f775
ron <138569343+ronaldw07@users.noreply.github.com>
2026-06-04 19:47:23 -0700

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        l = 0
+        charset = set()
+        longest = 0
+
+        #increase window until duplicate character found
+
+        # abcabcd
+        for r in range(len(s)):
+            while s[r] in charset:
+                charset.remove(s[l])
+                l += 1
+
+
+
+
+            charset.add(s[r])
+            longest = max(longest, r - l + 1)
+
+        #charset = {a,b,c} - {b,c,a} - {c,a,b} - {a,b,c} - {a,b,c,d}
+        return longest
+
+
+
+
+            
+            
+
+            
